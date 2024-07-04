e8f4a338dfe77d43f45ff39ff964d2e734245db6
Harish S <harishs@Harishs-MacBook-Pro.local>
2024-07-04 19:11:12 +0530

    added scripts

---
+++ Ingest.py
@@ -0,0 +1,46 @@
+# META DATA - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    # Developer details: 
+        # Name: Harish S
+        # Role: Architect
+        # Code ownership rights: Harish S
+    # Version:
+        # Version: V 1.0 (July 1)
+            # Developer: Harish S
+     
+    # Description: This code enables CRUD operations in REDIS to store csv data
+    
+# CODE - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# Dependency: 
+    # Environment:     
+        #Python 3.10.13 -> pip install 
+        #redis 5.0.7 -> pip install redis
+        #Pandas 2.2.1 -> pip install Pandas==2.2.1
+
+# Import Libraries
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Load the data
+path=input("Enter the path to save csv:")
+
+# PostgreSQL connection details
+db_username = input('enter username:') #username created in postgres
+db_password = input('enter password:') #password provided in postgres
+db_host = "localhost"#localhost
+db_port = "5432"
+db_name = input('enter database name:') # our selected database name
+db_table =input('Enter the table name:') # our selected table name 
+
+# Create the connection
+engine = create_engine(f'postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}')
+
+# Read data from the PostgreSQL table into a pandas DataFrame
+df = pd.read_sql(f'SELECT * FROM {db_table}', engine)
+
+# Export the DataFrame to a CSV file
+df.to_csv(path+'Ingestpgres.csv', index=False)
+
+print("Data exported successfully.")
---
+++ loadpostgres.py
@@ -0,0 +1,59 @@
+# META DATA - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    # Developer details: 
+        # Name: Harish S
+        # Role: Architect
+        # Code ownership rights: Harish S
+    # Version:
+        # Version: V 1.0 (July 1)
+            # Developer: Harish S
+     
+    # Description: This code enables loading of data in to postgres
+    
+# CODE - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# Dependency: 
+    # Environment:     
+        #Python 3.10.13 -> pip install 
+        #redis 5.0.7 -> pip install redis
+        #Pandas 2.2.1 -> pip install Pandas==2.2.1
+
+# Import Libraries
+
+import pandas as pd #data manipulation library
+import psycopg2 #driver library from python 
+from sqlalchemy import create_engine
+
+# Load the data
+path=input("Enter the path of csv:")
+df = pd.read_csv(path+'carsdata.csv')  # Adjust the path to your CSV file
+
+# Define database connection parameters
+db_name =input("enter database_name:") #enter database name
+db_user = input("enter username: ") #enter username
+db_password =input("enter password: ") # password for the user entered
+db_host = "localhost"
+db_port = "5432"  # defaults to 5432 for PostgreSQL
+
+# Connect to the PostgreSQL database
+conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_password, host=db_host, port=db_port)
+cur = conn.cursor()
+
+# Define SQL statement
+sql = """
+INSERT INTO CarData (Car_Make,Country,Country_Code)
+VALUES (%s,%s,%s)
+"""
+
+# Iterate through each row in the CSV data (replace with your logic)
+for row in df.to_records(index=False):  # Assuming using pandas
+    cur.execute(sql, row)
+
+# Commit the changes to the database
+conn.commit()
+
+# Close the connection
+cur.close()
+conn.close()
+
+print("CSV data imported successfully!")
